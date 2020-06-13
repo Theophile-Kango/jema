337578eb1692baf3e20a60a1293d4a0d4dd4bed8
Theophile-Kango <fadhili.kango@gmail.com>
2020-06-13 08:46:24 +0200

    Setup the model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :destroy
     has_many :like_posts, dependent: :destroy
+    has_many :taggings
+    has_many :tags, through: :taggings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_060950) do
+ActiveRecord::Schema.define(version: 2020_06_13_063911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,11 +65,25 @@ ActiveRecord::Schema.define(version: 2020_06_11_060950) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_taggings_on_post_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -91,4 +105,6 @@ ActiveRecord::Schema.define(version: 2020_06_11_060950) do
   add_foreign_key "like_comments", "users"
   add_foreign_key "like_posts", "posts"
   add_foreign_key "like_posts", "users"
+  add_foreign_key "taggings", "posts"
+  add_foreign_key "taggings", "tags"
 end
